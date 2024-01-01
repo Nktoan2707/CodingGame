cba5127fbb96050c709933db33d8f1a9c975fd4e
Nktoan2707 <nktoan20022707@gmail.com>
2024-01-01 14:27:09 +0700

    remove number of gems field
    - now we will use the count function of List gemList

--- Assets/Code/Scripts/ScriptableObjects/GameLevelSO.cs
+++ Assets/Code/Scripts/ScriptableObjects/GameLevelSO.cs
@@ -8,9 +8,8 @@ public class GameLevelSO : ScriptableObject
     public string sceneName;
     public string category;
     public int level;
-    public int numberOfGems;
-    public List<Vector3> gemPositionList;
     public Vector3 initialPlayerPosition;
     public Vector2 initialPlayerMovingDirection;
+    public List<Vector3> gemPositionList;
     public List<ActionName> actionNameList;
 }
