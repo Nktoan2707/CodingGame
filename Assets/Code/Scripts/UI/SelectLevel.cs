f4753b09eaf2df336ab9e3fe2090b3d31de2710f
Trương Trọng Khánh <82425500+trongkhanh11@users.noreply.github.com>
2024-01-03 14:22:44 +0700

    fix bug UI

--- Assets/Code/Scripts/UI/SelectLevel.cs
+++ Assets/Code/Scripts/UI/SelectLevel.cs
@@ -19,22 +19,12 @@ public class SelectLevel : MonoBehaviour
 
     public void LoadLevel(GameLevelSO gameLevelSO)
     {
-<<<<<<< Updated upstream:Assets/Code/Scripts/UI/SelectLevel.cs
-        SceneManager.UnloadSceneAsync("Scene_Main_Menu");
-        SceneManager.LoadSceneAsync("Scene_Basic_Select_Chapter", LoadSceneMode.Additive);
-=======
         ActionSceneManager.currentGameLevelSO = gameLevelSO;
         SceneManager.LoadSceneAsync("Action_Screen");
->>>>>>> Stashed changes:Assets/Code/Scripts/SystemScripts/BasicScreen/SelectLevel.cs
     }
 
     public void UnloadSelectLevel()
     {
-<<<<<<< Updated upstream:Assets/Code/Scripts/UI/SelectLevel.cs
-        SceneManager.UnloadSceneAsync("Scene_Basic_Select_Chapter");
-        SceneManager.LoadSceneAsync("Scene_Main_Menu", LoadSceneMode.Additive);
-=======
         SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex - 1);
->>>>>>> Stashed changes:Assets/Code/Scripts/SystemScripts/BasicScreen/SelectLevel.cs
     }
 }
